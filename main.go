67fffdeeb4cedf552383dd714c06a84738ac4348
Ernad Husremovic <hernad@bring.out.ba>
2024-11-27 12:12:47 +0100

    github.com/hernad/gomod2nix/templates/app

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/hernad/gomod2nix/internal/cmd"
+import (
+  "github.com/hernad/gomod2nix/internal/cmd"
+  "github.com/hernad/gomod2nix/templates/app"
+)
 
 func main() {
 	cmd.Execute()
